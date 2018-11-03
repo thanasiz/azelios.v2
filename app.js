ab5251036f61ca89b0d13d63f2a1106dcf411679
thanasiz <athanatos_zelios@hotmail.com>
2018-11-04 00:35:53 +0200

    latest update from live

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
 var express = require("express");
 var app = express();
-//const port = 3000
+var helmet = require("helmet");
 
-//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-//var fs = require('fs');
-//var http = require('http');
-//var https = require('https');
-//var privateKey  = fs.readFileSync('certificates/key.pem', 'utf8');
-//var certificate = fs.readFileSync('certificates/cert.pem', 'utf8');
-
-//var credentials = {key: privateKey, cert: certificate};
-
-//var httpServer = http.createServer(app);
-//var httpsServer = https.createServer(credentials, app);
-
-// For http
-//httpServer.listen(8080);
-// For https
-//httpsServer.listen(8443);
-
-
+//use "helmet" for security headers
+app.use(helmet());
 //express to use css files in public folder
 app.use(express.static("public"));
 //express to use images in img folder
@@ -32,13 +15,12 @@ app.use(express.static("other"));
 //remove ejs suffix from files
 app.set("view engine", "ejs");
 
-//"/tags/:title"
-// app.get("/tags/:tagName/:titleOfArticle", function(req, res) {
-//     res.send("tag");
-//     console.log("description area");
-// });
+// /tags/uncertainty-scaling-human-systems
+app.get("/tags/uncertainty-scaling-human-systems", function(req, res) {
+    res.render("tags/uncertainty-scaling-human-systems");
+})
 
-//"/tags/:title" blog posts
+// /tags/customer-support-engineer-product-manager
 app.get("/tags/customer-support-engineer-product-manager", function(req, res) {
     res.render("tags/customer-support-engineer-product-manager");
 })
